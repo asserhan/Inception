f5b91ae3a5f63636a9ef41ad4df7909ec93d7f2b
hasserao <asseraouhanane@gmail.com>
2025-01-09 18:57:22 +0100

    fix mariadb database acces to root

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,26 +1,13 @@
 #!/bin/bash
 
+mysqld_safe &
+sleep 5
 
-#start mysql
-service mysql start
+mysql -u root -p"${SQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`$SQL_DB\`;"
+mysql -u root -p"${SQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
+mysql -u root -p"${SQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON \`$SQL_DB\`.* TO '$SQL_USER'@'%';"
+mysql -u root -p"${SQL_ROOT_PASSWORD}" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';"
+mysql -u root -p"${SQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
 
-# Create a new database
-mysql -e "CREATE DATABASE IF NOT EXISTS \ `${SQL_DB}\ `;"
-
-# Create a new user
-mysql -e "CREATE USER IF NOT EXISTS \ `${SQL_USER}\ `@'localhost' IDENTIFIED BY '${SQL_PASS}';"
-
-# Grant privileges to the user
-mysql -e "GANT ALL PRIVILEGES ON \ `${SQL_DB}\ `.* TO \ `${SQL_USER}\ `@'%' IDENTIFIED BY '${SQL_PASS}';"
-
-# change the password of the root user
-mysql -e "ALTER USER 'root '@'localhost' IDENTIFIED BY '${SQL_ROOT_PASS}';"
-
-# Flush privileges It ensures that any changes made to user accounts take effect immediately without requiring a server restart.
-mysql -e "FLUSH PRIVILEGES;"
-
-#restart mysql
-mysqladmin -u root -p${SQL_ROOT_PASS} shutdown
-
-#start mysql
+mysqladmin -u root -p"$SQL_ROOT_PASSWORD" shutdown
 mysqld_safe
--- srcs/requirements/wordpress/tools/php-fpm.sh
+++ srcs/requirements/wordpress/tools/php-fpm.sh
@@ -9,14 +9,14 @@ if ! wp --allow-root --version; then
     mv wp-cli.phar /usr/local/bin/wp
 fi
 
-if [ ! -e /var/www/wordpress/wp-config.php ]; then
-    wp config create --allow-root --dbname=$SQL_DB --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306 --path='/var/www/wordpress'
-    
-    wp core install --allow-root --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --path='/var/www/wordpress'
-    
-    # Create WordPress author user
-    wp user create --allow-root --role=author $AUTHOR_USER $AUTHOR_EMAIL --user_pass=$AUTHOR_PASSWORD --path='/var/www/wordpress'
-fi
+# if [ ! -e /var/www/wordpress/wp-config.php ]; then
+wp config create --allow-root --dbname=$SQL_DB --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb:3306 --path='/var/www/html'
+
+wp core install --allow-root --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --path='/var/www/html'
+
+# # Create WordPress author user
+wp user create --allow-root --role=author $AUTHOR_USER $AUTHOR_EMAIL --user_pass=$AUTHOR_PASSWORD --path='/var/www/html'
+# fi
 
 # Ensure the /run/php directory exists
 if [ ! -d /run/php ]; then
